57e67198816c5b9273418d12aad29b5be6a65b64
Marco Yeung <myeung@redhat.com>
2022-07-08 13:01:38 -0400

    tinkering webhooks - algocoding

--- main.go
+++ main.go
@@ -110,6 +110,10 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Redisdb")
 		os.Exit(1)
 	}
+	if err = (&cachev1alpha1.AlgoCoding{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "AlgoCoding")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
